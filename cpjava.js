48bdf7d9689cd14eaa7aa6179ddde99991c66f27
Zachary Young <zacharyyoung@Zacharys-MacBook-Pro-2.local>
2018-02-15 23:30:11 -0700

    improved look of code

--- cpjava.js
+++ cpjava.js
@@ -4,28 +4,27 @@ $(document).ready(function() {
 		e.preventDefault();
 		newsValue = $("#newsInput").val();
 		console.log(newsValue);
-	var url = 'https://newsapi.org/v2/everything?' + 'q=' + newsValue + "&" + 'from=2018-02-16&' + 'sortBy=popularity&' + 'apiKey=43cd043466984ada8c4d6ac84c2df3f4';
-	$.ajax({
-		url : url,
-		    dataType : "json",
-		    success : function(json) {
-		    console.log(json);
-		    var article = " ";
-		    var newsArticleName  = "";
-		    for (var i = 0; i < 5; ++i) {
-			newsArticleName = json.articles[i].source.name;
-			console.log(newsArticleName);
-			var usNews = '<h2>News story from: ' + newsArticleName + "</h2>";
-			usNews += '<h5>Author: ' + json.articles[i].author + "</h5>";
-			usNews += '<p>Description: '+ json.articles[i].description + "</p>";
-			usNews += '<img src=' + json.articles[i].urlToImage + ">"
-			var articleUrl = json.articles[i].url;
-			//articleUrl = console.log(json.articles[i].url);
-			usNews += '<p>The URL for the article (have to copy and paste):<br>' + articleUrl + "</p>";
-			//articleUrl = console.log(json.articles[0].url);
-			$("#userPickedArticle"+ i).html(usNews);
-		    }
-		}
-	    });
+		var url = 'https://newsapi.org/v2/everything?' + 'q=' + newsValue + "&" + 
+		    'from=2018-02-16&' + 'sortBy=popularity&' + 'apiKey=43cd043466984ada8c4d6ac84c2df3f4';
+		$.ajax({
+			url : url,
+			dataType : "json",
+			success : function(json) {
+			    console.log(json);
+			    var article = " ";
+			    var newsArticleName  = "";
+			    for (var i = 0; i < 5; ++i) {
+				newsArticleName = json.articles[i].source.name;
+				console.log(newsArticleName);
+				var usNews = '<h2>News story from: ' + newsArticleName + "</h2>";
+				usNews += '<h5>Author: ' + json.articles[i].author + "</h5>";
+				usNews += '<p>Description: '+ json.articles[i].description + "</p>";
+				usNews += '<img src=' + json.articles[i].urlToImage + ">"
+				var articleUrl = json.articles[i].url;
+				usNews += '<p>The URL for the article (have to copy and paste):<br>' + articleUrl + "</p>";
+				$("#userPickedArticle"+ i).html(usNews);
+			    }
+			}
+	       });
        });
 });
